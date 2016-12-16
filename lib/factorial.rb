e3eaaf22e50bde22c41eb3f4c0114c1cd57917aa
Lewis Brown <lewisbrown@gmail.com>
2016-12-15 20:01:31 -0800

    cleaned up factorial; specs

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -3,3 +3,7 @@ module Factorial
     (1..self).reduce(:*) || 1
   end
 end
+
+class Integer
+  include Factorial
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,41 +1,33 @@
 require "randomgrams/anagrams"
 require "factorial"
 
-class Integer
-  include Factorial
-end
-
 RSpec.describe "Anagrams" do
-  TESTWORD = "life" 
+  TESTWORD = "life"
 
   context "object methods" do
-    let(:a) { Anagrams.new }
-
     it "should compute the permutations of word" do
       candidates = Anagrams.permute TESTWORD
-      #candidates = Anagrams.send :permute, TESTWORD
       expect(candidates.length).to eq TESTWORD.length.fact
       expect(candidates).to include "file"
       expect(candidates).to include "lief"
     end
 
     it "should find any anagram in the dictionary" do
-      candidates = Anagrams.permute TESTWORD
-      anagrams = Anagrams.find_words candidates
-      expect(anagrams).to include "file"
-      expect(anagrams).to include "lief"
-      expect(anagrams.length).to be 4
+      candidate_anagrams = ["beach", "river", "pond"]
+      anagrams = Anagrams.find_words candidate_anagrams
+      expect(anagrams).to include "beach"
+      expect(anagrams).to include "river"
     end
 
     it "should return a set of english anagrams for a word" do
-      anagrams = a.for(TESTWORD)
+      anagrams = Anagrams.new.for(TESTWORD)
       expect(anagrams).to include "file"
       expect(anagrams).to include "lief"
       expect(anagrams.length).to be 4
     end
   end
 
-  context "class methods" do
+  context "class method" do
     it "should return a set of english anagrams for a word" do
       a = Anagrams.for(TESTWORD)
       expect(a).to include "file"
